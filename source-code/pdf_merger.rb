ed8ea9a7986a3bb5cb1171afb41e953ec743fb8b
Elisha Lai <elisha.lai@outlook.com>
2015-10-28 02:28:19 -0400

    Update pdf_merger.rb

--- source-code/pdf_merger.rb
+++ source-code/pdf_merger.rb
@@ -9,15 +9,15 @@
 require 'combine_pdf'
 
 text_file = ARGV[0]
-combined_pdf_file = ARGV[1]
+combined_pdf_file = ARGV[1] + '.pdf'
 combined_pdf = CombinePDF.new
 
 IO.foreach(text_file) do |line|
-   individual_pdf_file = "#{line}.pdf"
-   puts "Loading #{individual_pdf_file.chomp} into the combined PDF."
+   individual_pdf_file = "#{line.chomp}.pdf"
+   puts "Loading #{individual_pdf_file} into #{combined_pdf_file}."
    combined_pdf << CombinePDF.load(individual_pdf_file)
 end
 
-puts 'Saving the combined PDF to the current directory.'
+puts "Saving #{combined_pdf_file} to the current directory."
 combined_pdf.save(combined_pdf_file)
-puts 'Combined PDF successfully saved.'
+puts "#{combined_pdf_file} successfully saved."
